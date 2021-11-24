65173d2d5d637d82e6d71a18eed900fc1133125a
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-11-24 12:06:38 +0200

    learn new components

--- WPF_Study/MainWindow.xaml.cs
+++ WPF_Study/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -33,6 +34,17 @@ namespace WPF_Study
             //button.Click += btm1_Click;
 
 
+            //ListBox1.Items.Add(new Student
+            //{
+            //    Name ="Алекс-Тейлор", 
+            //    Bd = new DateTime(2003, 02, 12)
+            //});
+
+            //ListBox1.Items.Add(new Student
+            //{
+            //    Name = "Марк",
+            //    Bd = new DateTime(2002, 08, 20)
+            //});
 
         }
 
@@ -42,16 +54,29 @@ namespace WPF_Study
             MessageBox.Show("HELLO!!!");
         }
 
-        public class Student
+
+        //private void ListBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    MessageBox.Show(ListBox1.SelectedItem.ToString());
+        //}
+
+        //private void b2_MouseEnter(object sender, MouseEventArgs e)
+        //{
+        //    p1.IsOpen = true;
+        //}
+
+    }
+
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public DateTime Bd { get; set; } = DateTime.Now;
+
+
+        public override string ToString()
         {
-            public string Name { get; set; }
-            public DateTime Bd { get; set; }
-
-
-            public override string ToString()
-            {
-                return $"{Name} | {Bd.ToShortDateString()}";
-            }
+            return $"{Name} | {Bd.ToShortDateString()}";
         }
     }
 }
