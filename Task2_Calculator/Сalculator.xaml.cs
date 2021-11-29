cab5b552608b3432b5b1db7ebfd384c187857f94
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-11-29 10:49:10 +0200

    Full refactoring

--- Task2_Calculator/Сalculator.xaml.cs
+++ Task2_Calculator/Сalculator.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Task2_Calculator
 {
@@ -20,189 +11,179 @@ namespace Task2_Calculator
     /// </summary>
     public partial class Сalculator : Window
     {
-        private double first;
-        private double second;
-
-        private char symbol;
-        private bool isFirst = true;
+        private string _expression = "";
+        private string Expression { get => _expression; set { _expression = value; OnExpressionChange(); } }
 
         public Сalculator()
         {
             InitializeComponent();
+            Closed += (s, e) => Application.Current.Shutdown(0);
         }
+
+        private void OnExpressionChange()
+        {
+            TextBlockFirst.Text = Expression;
+        }
+
+
+        #region Кнопки 0-9
+
+        private bool EvalRealNum()
+        {
+            int i = Expression.Length - 1;
+            if (Expression.Length != 0 && Expression.Last() == '0')
+            {
+                for (; i >= 0; i--)
+                {
+                    if (Expression[i] == '.')
+                        break;
+                }
+                if (i == -1) return false;
+            }
+            return true;
+        }
+
+        private void Button_0_Click(object sender, RoutedEventArgs e)
+        {
+            int i = Expression.Length - 1;
+            if (Expression.Length != 0 && Expression.Last() == '0')
+            {
+                for (; i >= 0; i--)
+                {
+                    if (Regex.IsMatch(Expression[i].ToString(), @"[/*+-]"))
+                        return;
+                    if (Regex.IsMatch(Expression[i].ToString(), @"[1-9]") || Expression[i] == '.')
+                        break;
+                }
+                if (i == -1) return;
+
+            }
+            Expression += '0';
+        }
+
+        private void Button_1_Click(object sender, RoutedEventArgs e)
+        {
+            if (EvalRealNum())
+                Expression += '1';
+        }
+
+        private void Button_2_Click(object sender, RoutedEventArgs e)
+        {
+            if (EvalRealNum())
+                Expression += '2';
+        }
+
+        private void Button_3_Click(object sender, RoutedEventArgs e)
+        {
+            if (EvalRealNum())
+                Expression += '3';
+        }
+
+        private void Button_4_Click(object sender, RoutedEventArgs e)
+        {
+            if (EvalRealNum())
+                Expression += '4';
+        }
+
+        private void Button_5_Click(object sender, RoutedEventArgs e)
+        {
+            if (EvalRealNum())
+                Expression += '5';
+        }
+
+        private void Button_6_Click(object sender, RoutedEventArgs e)
+        {
+            if (EvalRealNum())
+                Expression += '6';
+        }
+
+        private void Button_7_Click(object sender, RoutedEventArgs e)
+        {
+            if (EvalRealNum())
+                Expression += '7';
+        }
+
+        private void Button_8_Click(object sender, RoutedEventArgs e)
+        {
+            if (EvalRealNum())
+                Expression += '8';
+        }
+
+        private void Button_9_Click(object sender, RoutedEventArgs e)
+        {
+            if (EvalRealNum())
+                Expression += '9';
+        }
+
+        #endregion
+
+
+        #region Операторы CE, C, <, ., +, -, *, /
 
         private void Button_CE_Click(object sender, RoutedEventArgs e)
         {
-            TextBlockFirst.Text = "";
+            Expression = "";
         }
 
         private void Button_C_Click(object sender, RoutedEventArgs e)
         {
-            TextBlockFirst.Text = "";
+            Expression = "";
             TextBlockLast.Text = "";
         }
 
         private void Button_LT_Click(object sender, RoutedEventArgs e)
         {
-            if (TextBlockFirst.Text.Length != 0)
-                TextBlockFirst.Text = TextBlockFirst.Text.Remove(TextBlockFirst.Text.Length - 1);
+            if (Expression.Length != 0)
+                Expression = Expression.Remove(Expression.Length - 1);
         }
-
-    
-
-        #region Кнопки 0-9
-
-        private void Button_0_Click(object sender, RoutedEventArgs e)
-        {
-            TextBlockFirst.Text += "0";
-        }
-
-        private void Button_1_Click(object sender, RoutedEventArgs e)
-        {
-            TextBlockFirst.Text += "1";
-        }
-
-        private void Button_2_Click(object sender, RoutedEventArgs e)
-        {
-            TextBlockFirst.Text += "2";
-        }
-
-        private void Button_3_Click(object sender, RoutedEventArgs e)
-        {
-            TextBlockFirst.Text += "3";
-        }
-
-        private void Button_4_Click(object sender, RoutedEventArgs e)
-        {
-            TextBlockFirst.Text += "4";
-        }
-
-        private void Button_5_Click(object sender, RoutedEventArgs e)
-        {
-            TextBlockFirst.Text += "5";
-        }
-
-        private void Button_6_Click(object sender, RoutedEventArgs e)
-        {
-            TextBlockFirst.Text += "6";
-        }
-
-        private void Button_7_Click(object sender, RoutedEventArgs e)
-        {
-            TextBlockFirst.Text += "7";
-        }
-
-        private void Button_8_Click(object sender, RoutedEventArgs e)
-        {
-            TextBlockFirst.Text += "8";
-        }
-
-        private void Button_9_Click(object sender, RoutedEventArgs e)
-        {
-            TextBlockFirst.Text += "9";
-        }
-
-        #endregion
-
-
-        #region Операторы
 
         private void Button_Dot_Click(object sender, RoutedEventArgs e)
         {
-            TextBlockFirst.Text += ".";
+            if (Expression.Length != 0 && Regex.IsMatch(Expression.Last().ToString(), @"\d"))
+                Expression += '.';
         }
 
         private void Button_Plus_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                first = Double.Parse(TextBlockFirst.Text);
-                TextBlockFirst.Text += "+";
-                symbol = '+';
-            }
-            catch (Exception exception)
-            {
-                MessageBox.Show("Ошибка!", exception.Message, MessageBoxButton.OK, MessageBoxImage.Error);
-            }
+            if (Expression.Length != 0 && !Regex.IsMatch(Expression.Last().ToString(), @"[*/+-.]"))
+                Expression += '+';
         }
 
         private void Button_Minus_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                first = Double.Parse(TextBlockFirst.Text);
-                TextBlockFirst.Text += "-";
-                symbol = '-';
-            }
-            catch (Exception exception)
-            {
-                MessageBox.Show("Ошибка!", exception.Message, MessageBoxButton.OK, MessageBoxImage.Error);
-            }
+            if (Expression.Length == 0 || !Regex.IsMatch(Expression.Last().ToString(), @"[*/+-.]"))
+                Expression += '-';
         }
 
         private void Button_Multi_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                first = Double.Parse(TextBlockFirst.Text);
-
-                TextBlockFirst.Text += "*";
-                symbol = '*';
-            }
-            catch (Exception exception)
-            {
-                MessageBox.Show("Ошибка!", exception.Message, MessageBoxButton.OK, MessageBoxImage.Error);
-            }
+            if (Expression.Length != 0 && !Regex.IsMatch(Expression.Last().ToString(), @"[*/+-.]"))
+                Expression += '*';
         }
 
         private void Button_Divide_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                first = Double.Parse(TextBlockFirst.Text);
-                TextBlockFirst.Text += "/";
-                symbol = '/';
-            }
-            catch (Exception exception)
-            {
-                MessageBox.Show("Ошибка!", exception.Message, MessageBoxButton.OK, MessageBoxImage.Error);
-            }
+            if (Expression.Length != 0 && !Regex.IsMatch(Expression.Last().ToString(), @"[*/+-.]"))
+                Expression += "/";
         }
 
         #endregion
 
-        public void Сount()
-        {
-            switch (symbol)
-            {
-                case '/':
-                    TextBlockFirst.Text = (first / second).ToString();
-                    break;
-                case '*':
-                    TextBlockFirst.Text = (first * second).ToString();
-                    break;
-                case '+':
-                    TextBlockFirst.Text = (first + second).ToString();
-                    break;
-                case '-':
-                    TextBlockFirst.Text = (first - second).ToString();
-                    break;
-            }
 
-        }
-
-        private void Button_Equals_Click(object sender, RoutedEventArgs e)
+        private void Button_Equals_Click(object sender, RoutedEventArgs e) // =
         {
+            double result;
             try
             {
-                second = Double.Parse(TextBlockFirst.Text.Split(symbol)[1]);
-                TextBlockLast.Text = TextBlockFirst.Text;
-                Сount();
+                result = Convert.ToDouble(new DataTable().Compute(TextBlockFirst.Text, null));
             }
-            catch (Exception exception)
+            catch (Exception ex)
             {
-                MessageBox.Show("Ошибка!", exception.Message, MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
             }
+            TextBlockLast.Text = TextBlockFirst.Text;
+            Expression = result.ToString();
+            TextBlockFirst.Text = result.ToString();
         }
 
    
