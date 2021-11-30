15765f407020b6149780f57ca8eb5e775d8ee6c9
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-11-30 21:38:52 +0200

    + dynamic choose theme

---
+++ WPF_Study_ServerManager/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Study_ServerManager
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void themeDark_Checked(object sender, RoutedEventArgs e)
+        {
+            ResourceDictionary themeDictionary = new ResourceDictionary();
+            themeDictionary.Source = new Uri("DarkTheme.xaml", UriKind.Relative);
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(themeDictionary);
+        }
+
+        private void themeLight_Checked(object sender, RoutedEventArgs e)
+        {
+            ResourceDictionary themeDictionary = new ResourceDictionary();
+            themeDictionary.Source = new Uri("LightTheme.xaml", UriKind.Relative);
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(themeDictionary);
+        }
+    }
+}
