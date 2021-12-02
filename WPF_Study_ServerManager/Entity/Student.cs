953811c5049f0370b808bfd5d1713ea3dd0c9881
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-12-02 18:56:24 +0200

    Create class Student

---
+++ WPF_Study_ServerManager/Entity/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace WPF_Study_ServerManager.Entity
+{
+    public class Student : Person
+    { 
+        public int Progress { get; set; }
+    }
+}
