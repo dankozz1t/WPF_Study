57367fda60eeab9e73b9e67440b7013214d8c180
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-12-02 18:56:06 +0200

    Create class Group

---
+++ WPF_Study_ServerManager/Entity/Group.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace WPF_Study_ServerManager.Entity
+{
+    public class Group
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public List<Student> Students { get; set; } = new List<Student>();
+        public Teacher Teacher { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
