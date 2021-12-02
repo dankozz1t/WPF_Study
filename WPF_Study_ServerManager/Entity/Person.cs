0de5e395f96a1e8d3c964ffd0ac509332285e1a4
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-12-02 18:56:12 +0200

    Create class Person

---
+++ WPF_Study_ServerManager/Entity/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WPF_Study_ServerManager.Entity
+{
+    public class Person
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + " " + SecondName;
+        }
+    }
+}
