7eff583f0df6fc2ec758a4820123a154e490c98a
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-12-06 13:26:14 +0200

    + add Entity

---
+++ WPF_Study_ServerManager/DBGroupProj/AddEntity.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace WPF_Study_ServerManager.DBGroupProj.DataBase
+{
+    /// <summary>
+    /// Логика взаимодействия для AddGroup.xaml
+    /// </summary>
+    public partial class AddEntity : Window
+    {
+        public string FirstName { get => StudentFirstName.Text; set => StudentFirstName.Text = value; }
+        public string SecondName { get => StudentSecondName.Text; set => StudentSecondName.Text = value; }
+        public string BonusField { get =>StudentBonusField.Text; set => StudentBonusField.Text = value; }
+
+        public string BonusEntityText { set => BonusTextBlock.Text = value; }
+
+        public AddEntity()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Ok_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void Button_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
