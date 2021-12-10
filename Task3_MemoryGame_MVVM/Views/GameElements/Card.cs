f044771501e709bdff3497ea9a6fa2f46281326d
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-12-10 09:18:42 +0200

    Create Card.cs

---
+++ Task3_MemoryGame_MVVM/Views/GameElements/Card.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using Task3_MemoryGame_MVVM.Data;
+using Task3_MemoryGame_MVVM.ViewModels;
+
+namespace Task3_MemoryGame_MVVM.GameElements
+{
+    public class Card : Button
+    {
+        public int CardIndex { get; set; } = 0;
+        public bool isOpen { get; set; } = false;
+
+        public Card(int CardIndex):base()
+        {
+            this.CardIndex = CardIndex;
+
+            Image img = new Image();
+            img.Source = new BitmapImage(new Uri(DBContext.getInstance().Images[CardIndex].FileName));
+            img.Margin = new Thickness(15);
+
+            this.Content = img;
+
+        }
+    }
+}
