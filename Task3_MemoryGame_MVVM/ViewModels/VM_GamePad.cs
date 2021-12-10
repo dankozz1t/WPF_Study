893407d285c215d6478deb7470cf501a6227623e
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-12-10 18:37:54 +0200

    rename

--- Task3_MemoryGame_MVVM/ViewModels/VM_GamePad.cs
+++ Task3_MemoryGame_MVVM/ViewModels/VM_GamePad.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Windows.Controls;
 using Task3_MemoryGame_MVVM.Annotations;
 using Task3_MemoryGame_MVVM.Data;
 using Task3_MemoryGame_MVVM.GameElements;
@@ -13,54 +12,55 @@ namespace Task3_MemoryGame_MVVM.ViewModels
     {
         public Card openCard { get; set; } = null;
 
-        public int Rows { get; set; } = 5;
-        public int Cols { get; set; } = 8;
+        public int Rows { get; set; }
+        public int Cols { get; set; }
 
         public int[,] Cards { get; set; }
 
         private void BuildGamePad()
         {
             Cards = new int[Rows, Cols];
-            Random rnd = new Random();
+            Random random = new Random();
 
             bool isPair = true;
             int curCard = 0;
 
-            List<Entities.Image> lst = DBContext.getInstance().Images;
+            List<Entities.Image> images = DBContext.getInstance().Images;
 
-            for (int r = 0; r < Rows; r++)
+            for (int row = 0; row < Rows; row++)
             {
-                for (int c = 0; c < Cols; c++)
+                for (int colomn = 0; colomn < Cols; colomn++)
                 {
                     if (isPair)
                     {
-                        curCard = rnd.Next(lst.Count - 1) + 1;
+                        curCard = random.Next(images.Count - 1) + 1;
                         isPair = true;
                     }
 
-                    Cards[r, c] = curCard;
+                    Cards[row, colomn] = curCard;
                     isPair = !isPair;
                 }
             }
 
-            for (int toRand = 0; toRand < Rows * Cols; toRand++)
+            for (int i = 0; i < Rows * Cols; i++) //Перемешивает карточки
             {
-                int r1 = rnd.Next(Rows); int r2 = rnd.Next(Rows);
-                int c1 = rnd.Next(Cols); int c2 = rnd.Next(Cols);
+                int r1 = random.Next(Rows); int r2 = random.Next(Rows);
+                int c1 = random.Next(Cols); int c2 = random.Next(Cols);
                 (Cards[r1, c1], Cards[r2, c2]) = (Cards[r2, c2], Cards[r1, c1]);
             }
-
         }
 
-        public VM_GamePad()
+        public VM_GamePad(int rows, int columns)
         {
+            Rows = rows;
+            Cols = columns;
             BuildGamePad();
-
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         [NotifyPropertyChangedInvocator]
+
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
