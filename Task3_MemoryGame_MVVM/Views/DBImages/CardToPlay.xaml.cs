423db2375604f721d134fd859243ca02617bc83a
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-12-10 09:18:35 +0200

    Create card to play

---
+++ Task3_MemoryGame_MVVM/Views/DBImages/CardToPlay.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Task3_MemoryGame_MVVM.Data;
+
+namespace Task3_MemoryGame_MVVM.Views.DBImages
+{
+    /// <summary>
+    /// Логика взаимодействия для CardToPlay.xaml
+    /// </summary>
+    public partial class CardToPlay : Window
+    {
+        public CardToPlay()
+        {
+            InitializeComponent();
+            DataContext = DBContext.getInstance();
+        }
+    }
+}
