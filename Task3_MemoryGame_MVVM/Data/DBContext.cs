0206c62a4b22b56ccde5e693b4e18fe253df24bb
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-12-10 18:36:50 +0200

    rename

--- Task3_MemoryGame_MVVM/Data/DBContext.cs
+++ Task3_MemoryGame_MVVM/Data/DBContext.cs
@@ -18,17 +18,17 @@ namespace Task3_MemoryGame_MVVM.Data
             {
                 new Image{FileName = @""}, //Пустая карта
 
-                new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Public\Image\crown-3-256.png"},
-                new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Public\Image\discord-2-256.png"},
-                new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Public\Image\hot-chocolate-256.png"},
-                new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Public\Image\puzzle-4-256.png"},
-                new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Public\Image\radioactive-256.png"}
+                new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Resources\Image\crown-3-256.png"},
+                new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Resources\Image\discord-2-256.png"},
+                new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Resources\Image\hot-chocolate-256.png"},
+                new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Resources\Image\puzzle-4-256.png"},
+                new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Resources\Image\radioactive-256.png"}
 
-                //new Image{FileName = @"..\..\Public\Image\crown-3-256.png"},
-                //new Image{FileName = @"..\..\Public\Image\discord-2-256.png"},
-                //new Image{FileName = @"..\..\Public\Image\hot-chocolate-256.png"},
-                //new Image{FileName = @"..\..\Public\Image\puzzle-4-256.png"},
-                //new Image{FileName = @"..\..\Public\Image\radioactive-256.png"}
+                //new Image{FileName = @"..\..\Resources\Image\crown-3-256.png"},
+                //new Image{FileName = @"..\..\Resources\Image\discord-2-256.png"},
+                //new Image{FileName = @"..\..\Resources\Image\hot-chocolate-256.png"},
+                //new Image{FileName = @"..\..\Resources\Image\puzzle-4-256.png"},
+                //new Image{FileName = @"..\..\Resources\Image\radioactive-256.png"}
             };
         }
     }
