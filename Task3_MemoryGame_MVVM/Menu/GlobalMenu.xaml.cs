d9aede6b7eca3a9eb38961541c747091f7b9b069
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-12-10 18:36:18 +0200

    Create GlobalMenu

---
+++ Task3_MemoryGame_MVVM/Menu/GlobalMenu.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Task3_MemoryGame_MVVM.Views.GamePad;
+
+namespace Task3_MemoryGame_MVVM
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class GlobalMenu : Window
+    {
+        public GlobalMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Play_Click(object sender, RoutedEventArgs e)
+        {
+            GamePad play = new GamePad(Convert.ToInt32(RowsTextBlock.Text), Convert.ToInt32(ColumnsTextBlock.Text) );
+            play.Show();
+            this.Hide();
+        }
+
+        private void Button_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Slider_Columns_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ColumnsTextBlock.Text = Convert.ToInt32((sender as Slider).Value).ToString();
+        }
+
+        private void Slider_Rows_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            RowsTextBlock.Text = Convert.ToInt32((sender as Slider).Value).ToString();
+        }
+
+    }
+}
