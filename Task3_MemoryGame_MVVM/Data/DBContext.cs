08537b0564ba1e04a118671d374428dbf5accdd5
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-12-11 20:27:07 +0200

    rename path

--- Task3_MemoryGame_MVVM/Data/DBContext.cs
+++ Task3_MemoryGame_MVVM/Data/DBContext.cs
@@ -18,11 +18,11 @@ namespace Task3_MemoryGame_MVVM.Data
             {
                 new Image{FileName = @""}, //Пустая карта
 
-                new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Resources\Image\crown-3-256.png"},
-                new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Resources\Image\discord-2-256.png"},
-                new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Resources\Image\hot-chocolate-256.png"},
-                new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Resources\Image\puzzle-4-256.png"},
-                new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Resources\Image\radioactive-256.png"}
+                //new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Resources\Image\crown-3-256.png"},
+                //new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Resources\Image\discord-2-256.png"},
+                //new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Resources\Image\hot-chocolate-256.png"},
+                //new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Resources\Image\puzzle-4-256.png"},
+                //new Image{FileName = @"C:\Users\alexd\source\repos\WPF_Study\Task3_MemoryGame_MVVM\Resources\Image\radioactive-256.png"}
 
                 //new Image{FileName = @"..\..\Resources\Image\crown-3-256.png"},
                 //new Image{FileName = @"..\..\Resources\Image\discord-2-256.png"},
