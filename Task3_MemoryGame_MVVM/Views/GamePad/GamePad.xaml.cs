9356d8a5b6eb76b6af342cf4513ae56b401a5b7a
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-12-13 10:53:29 +0200

    refactoring

--- Task3_MemoryGame_MVVM/Views/GamePad/GamePad.xaml.cs
+++ Task3_MemoryGame_MVVM/Views/GamePad/GamePad.xaml.cs
@@ -1,96 +1,91 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Media;
 using System.Windows.Threading;
-using Task3_MemoryGame_MVVM.Entities;
-using Task3_MemoryGame_MVVM.GameElements;
-using Task3_MemoryGame_MVVM.ViewModels;
-using System.Windows.Threading;
+using MemoryGame.Entities;
+using MemoryGame.ViewModels;
+using MemoryGame.Views.GameElements;
 
-namespace Task3_MemoryGame_MVVM.Views.GamePad
+namespace MemoryGame.Views.GamePad
 {
     /// <summary>
     /// Логика взаимодействия для GamePad.xaml
     /// </summary>
     public partial class GamePad : Window
     {
-        private int attempts = 0;
-
-        private Label timerLabel = new Label();
-
-        private DispatcherTimer dt = new DispatcherTimer();
-        private Stopwatch sw = new Stopwatch();
-        private string currentTime = string.Empty;
+        private int _attempts = 1;
+        private readonly Label _timerLabel = new Label();
 
         public GamePad(int rows, int cols)
         {
             InitializeComponent();
 
-            DataContext = new ViewModels.VM_GamePad(rows, cols);
-            CreateGamePad();
+            DataContext = new MemoryGame.ViewModels.VMGamePad(rows, cols);
+
+            TimerStart();
+            BuildGamePad();
         }
 
-        void timer_Tick(object sender, EventArgs e)
+        #region Timer
+
+        private readonly DispatcherTimer _dt = new DispatcherTimer();
+        private readonly Stopwatch _sw = new Stopwatch();
+        private string _currentTime = string.Empty;
+
+        private void TimerStart()
         {
-            if (sw.IsRunning)
+            _dt.Tick += new EventHandler(timer_Tick);
+            _dt.Interval = new TimeSpan(0, 0, 0, 1);
+
+            _sw.Start();
+            _dt.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (_sw.IsRunning)
             {
-                TimeSpan ts = sw.Elapsed;
-                currentTime = String.Format("{0:00}:{1:00}", ts.Minutes, ts.Seconds);
-                timerLabel.Content = currentTime;
+                TimeSpan ts = _sw.Elapsed;
+                _currentTime = String.Format("{0:00}:{1:00}", ts.Minutes, ts.Seconds);
+                _timerLabel.Content = _currentTime;
             }
         }
 
-        private void CreateGamePad()
+        #endregion
+
+        #region BuildZone
+
+        private void BuildGamePad()
         {
-            dt.Tick += new EventHandler(timer_Tick);
-            dt.Interval = new TimeSpan(0, 0, 0, 1);
-
-            sw.Start();
-            dt.Start();
-
-            Grid myGrid = new Grid();
-            myGrid.HorizontalAlignment = HorizontalAlignment.Stretch;
-            myGrid.VerticalAlignment = VerticalAlignment.Stretch;
-
-            //myGrid.ShowGridLines = true;
-
-            if (DataContext is VM_GamePad viewGamePad)
+            this.Background = new SolidColorBrush(Colors.RosyBrown);
+            Grid mainGrid = new Grid
             {
-                RowDefinition topPanel = new RowDefinition();
-                topPanel.Height = new GridLength(60);
-                myGrid.RowDefinitions.Add(topPanel);
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Stretch
+            };
 
-                Grid topTimePanel = new Grid();
-                topTimePanel.Background = new SolidColorBrush(Colors.RosyBrown);
+            if (DataContext is VMGamePad viewGamePad)
+            {
+                RowDefinition topPanel = new RowDefinition { Height = new GridLength(60) };
+                mainGrid.RowDefinitions.Add(topPanel);
 
-                timerLabel.FontSize = 40;
-                timerLabel.HorizontalAlignment = HorizontalAlignment.Center;
-                timerLabel.VerticalAlignment = VerticalAlignment.Center;
-                timerLabel.Content = "00:00";
+                BuildTopTimerPanel(mainGrid, viewGamePad);
 
-                topTimePanel.Children.Add(timerLabel);
-
-                myGrid.Children.Add(topTimePanel);
-                Grid.SetRow(topTimePanel, 0);
-                Grid.SetColumnSpan(topTimePanel, 2);
-
-                for (int r = 0; r < viewGamePad.Rows; r++)
+                for (int row = 0; row < viewGamePad.Rows; row++)
                 {
-                    myGrid.RowDefinitions.Add(new RowDefinition());
+                    mainGrid.RowDefinitions.Add(new RowDefinition());
                 }
 
-                for (int c = 0; c < viewGamePad.Cols; c++)
+                for (int column = 0; column < viewGamePad.Columns; column++)
                 {
-                    myGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                    mainGrid.ColumnDefinitions.Add(new ColumnDefinition());
                 }
 
                 for (int row = 0; row < viewGamePad.Rows; row++)
                 {
-                    for (int column = 0; column < viewGamePad.Cols; column++)
+                    for (int column = 0; column < viewGamePad.Columns; column++)
                     {
                         Card card = new Card(viewGamePad.Cards[row, column]);
 
@@ -98,134 +93,163 @@ namespace Task3_MemoryGame_MVVM.Views.GamePad
                         {
                             (sender as Card).IsOpen = true;
 
-                            if (viewGamePad.openCard == null)
+                            if (viewGamePad.OpenCard == null) //Если открытой карты нет
                             {
-                                viewGamePad.openCard = (sender as Card);
+                                viewGamePad.OpenCard = (sender as Card);
                             }
                             else
                             {
-                                if (viewGamePad.openCard.CardIndex == (sender as Card).CardIndex)
+                                if (viewGamePad.OpenCard.CardIndex == (sender as Card).CardIndex) //Если открытые карты совпадают
                                 {
-                                    if(viewGamePad.openCard == (sender as Card))
+                                    if (viewGamePad.OpenCard == (sender as Card)) //Если дабл клик по одной карте
                                         return;
 
                                     AutoClosingMessageBox.Show("Отличная работа!", "ПОПАЛ!", 400);
 
-                                    myGrid.Children.Remove((sender as UIElement));
-                                    myGrid.Children.Remove((viewGamePad.openCard as UIElement));
+                                    mainGrid.Children.Remove((sender as UIElement));
+                                    mainGrid.Children.Remove((viewGamePad.OpenCard as UIElement));
 
-                                    if (myGrid.Children.Count == 1)
+                                    if (mainGrid.Children.Count == 1) //Если больше карт на панели нет
                                     {
-                                        //------------------------------------------------------
-                                        sw.Stop();
-
-                                        Grid gtidVictory = new Grid();
-                                        this.Background = new SolidColorBrush(Colors.RosyBrown);
-                                        gtidVictory.HorizontalAlignment = HorizontalAlignment.Center;
-                                        gtidVictory.VerticalAlignment = VerticalAlignment.Center;
-                                        gtidVictory.Height = 300;
-                                        gtidVictory.Width = 290;
-                                        gtidVictory.Background = new SolidColorBrush(Colors.PeachPuff);
-
-                                        RowDefinition rowsDefinition1 = new RowDefinition();
-                                        rowsDefinition1.Height = new GridLength(50);
-                                        gtidVictory.RowDefinitions.Add(rowsDefinition1);
-                                        gtidVictory.RowDefinitions.Add(new RowDefinition());
-                                        gtidVictory.RowDefinitions.Add(new RowDefinition());
-                                        RowDefinition rowsDefinition4 = new RowDefinition();
-                                        rowsDefinition4.Height = GridLength.Auto;
-                                        gtidVictory.RowDefinitions.Add(rowsDefinition4);
-
-                                        TextBlock topTextVictory = new TextBlock();
-                                        topTextVictory.Text = "ПОБЕДА!";
-                                        topTextVictory.HorizontalAlignment = HorizontalAlignment.Center;
-                                        topTextVictory.FontSize = 30;
-                                        topTextVictory.FontWeight = FontWeights.Bold;
-                                        gtidVictory.Children.Add(topTextVictory);
-                                        Grid.SetRow(topTextVictory, 0);
-
-                                        TextBlock textCountAttempts = new TextBlock();
-                                        textCountAttempts.Text = "Количество попыток:";
-                                        textCountAttempts.HorizontalAlignment = HorizontalAlignment.Center;
-                                        textCountAttempts.FontSize = 20;
-                                        textCountAttempts.FontWeight = FontWeights.Bold;
-
-                                        attempts++;
-                                        TextBlock textAttempts = new TextBlock();
-                                        textAttempts.Text = attempts.ToString();
-                                        textAttempts.HorizontalAlignment = HorizontalAlignment.Center;
-                                        textAttempts.FontSize = 20;
-                                        textAttempts.FontWeight = FontWeights.Bold;
-
-                                        StackPanel stack = new StackPanel();
-                                        stack.Children.Add(textCountAttempts);
-                                        stack.Children.Add(textAttempts);
-
-                                        gtidVictory.Children.Add(stack);
-                                        Grid.SetRow(stack, 1);
-
-
-                                        TextBlock textCountTime = new TextBlock();
-                                        textCountTime.Text = "Количество времени:";
-                                        textCountTime.HorizontalAlignment = HorizontalAlignment.Center;
-                                        textCountTime.FontSize = 20;
-                                        textCountTime.FontWeight = FontWeights.Bold;
-
-                                        attempts++;
-                                        TextBlock textTime = new TextBlock();
-                                        textTime.Text = currentTime;
-                                        textTime.HorizontalAlignment = HorizontalAlignment.Center;
-                                        textTime.FontSize = 20;
-                                        textTime.FontWeight = FontWeights.Bold;
-
-                                        StackPanel stack2 = new StackPanel();
-                                        stack2.Children.Add(textCountTime);
-                                        stack2.Children.Add(textTime);
-
-                                        gtidVictory.Children.Add(stack2);
-                                        Grid.SetRow(stack2, 2);
-
-
-                                        Button button = new Button();
-                                        button.Content = "НАЗАД";
-                                        gtidVictory.Children.Add(button);
-                                        Grid.SetRow(button, 3);
-
-
-                                        button.Click += (sender2, args2) =>
-                                        {
-                                            GlobalMenu globalMenu = new GlobalMenu();
-                                            globalMenu.Show();
-                                            this.Close();
-                                        };
-
-
-                                        this.Content = gtidVictory;
-                                        //------------------------------------------------------
+                                        _sw.Stop();
+                                        BuildVictoryPanel();
                                     }
                                 }
                                 else
                                 {
                                     AutoClosingMessageBox.Show("Попробуй еще!", "МИМО!", 400);
-                             
+
                                     (sender as Card).IsOpen = false;
-                                    (DataContext as VM_GamePad).openCard.IsOpen = false;
+                                    (DataContext as VMGamePad).OpenCard.IsOpen = false;
                                 }
 
-                                viewGamePad.openCard = null;
-                                attempts++;
+                                viewGamePad.OpenCard = null;
+                                _attempts++;
                             }
                         };
-
                         Grid.SetColumn(card, column);
-                        Grid.SetRow(card, row+1);
+                        Grid.SetRow(card, row + 1);
 
-                        myGrid.Children.Add(card);
+                        mainGrid.Children.Add(card);
                     }
                 }
-
             }
-            this.Content = myGrid;
+            this.Content = mainGrid;
         }
+
+        private void BuildTopTimerPanel(Grid mainGrid, VMGamePad gamePad)
+        {
+            Grid topTimePanel = new Grid();
+            topTimePanel.Background = new SolidColorBrush(Colors.RosyBrown);
+
+            _timerLabel.FontSize = 40;
+            _timerLabel.HorizontalAlignment = HorizontalAlignment.Center;
+            _timerLabel.VerticalAlignment = VerticalAlignment.Center;
+            _timerLabel.Content = "00:00";
+
+            topTimePanel.Children.Add(_timerLabel);
+
+            mainGrid.Children.Add(topTimePanel);
+            Grid.SetRow(topTimePanel, 0);
+            Grid.SetColumnSpan(topTimePanel, gamePad.Columns);
+        }
+
+        private void BuildVictoryPanel()
+        {
+            this.Background = new SolidColorBrush(Colors.RosyBrown);
+
+            Grid gtidVictory = new Grid
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Height = 300,
+                Width = 290,
+                Background = new SolidColorBrush(Colors.PeachPuff)
+            };
+
+            RowDefinition rowsDefinition1 = new RowDefinition { Height = new GridLength(50) };
+            gtidVictory.RowDefinitions.Add(rowsDefinition1);
+            gtidVictory.RowDefinitions.Add(new RowDefinition());
+            gtidVictory.RowDefinitions.Add(new RowDefinition());
+
+            RowDefinition rowsDefinition4 = new RowDefinition { Height = GridLength.Auto };
+            gtidVictory.RowDefinitions.Add(rowsDefinition4);
+
+            TextBlock topTextVictory = new TextBlock
+            {
+                Text = "ПОБЕДА!",
+                HorizontalAlignment = HorizontalAlignment.Center,
+                FontSize = 30,
+                FontWeight = FontWeights.Bold
+            };
+            gtidVictory.Children.Add(topTextVictory);
+            Grid.SetRow(topTextVictory, 0);
+
+            TextBlock textCountAttempts = new TextBlock
+            {
+                Text = "Количество попыток:",
+                HorizontalAlignment = HorizontalAlignment.Center,
+                FontSize = 20,
+                FontWeight = FontWeights.Bold
+            };
+
+            TextBlock textAttempts = new TextBlock
+            {
+                Text = _attempts.ToString(),
+                HorizontalAlignment = HorizontalAlignment.Center,
+                FontSize = 20,
+                FontWeight = FontWeights.Bold
+            };
+
+            StackPanel stackForAttempts = new StackPanel();
+            stackForAttempts.Children.Add(textCountAttempts);
+            stackForAttempts.Children.Add(textAttempts);
+
+            gtidVictory.Children.Add(stackForAttempts);
+            Grid.SetRow(stackForAttempts, 1);
+
+            TextBlock textCountTime = new TextBlock
+            {
+                Text = "Количество времени:",
+                HorizontalAlignment = HorizontalAlignment.Center,
+                FontSize = 20,
+                FontWeight = FontWeights.Bold
+            };
+
+            _attempts++;
+            TextBlock textTime = new TextBlock
+            {
+                Text = _currentTime,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                FontSize = 20,
+                FontWeight = FontWeights.Bold
+            };
+
+            StackPanel stackForTime = new StackPanel();
+            stackForTime.Children.Add(textCountTime);
+            stackForTime.Children.Add(textTime);
+
+            gtidVictory.Children.Add(stackForTime);
+            Grid.SetRow(stackForTime, 2);
+
+            Button buttonBack = new Button
+            {
+                Content = "НАЗАД"
+            };
+            gtidVictory.Children.Add(buttonBack);
+            Grid.SetRow(buttonBack, 3);
+
+            buttonBack.Click += (sender2, args2) =>
+            {
+                GlobalMenu globalMenu = new GlobalMenu();
+                globalMenu.Show();
+                this.Close();
+            };
+
+            this.Content = gtidVictory;
+        }
+
+        #endregion
+
     }
 }
