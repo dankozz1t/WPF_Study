f0f9cab10cb196eff54c2f931edd3db5a978ea5b
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-12-13 10:49:52 +0200

    renamed and cleaned

---
+++ Task3_MemoryGame_MVVM/Data/DataBaseContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using MemoryGame.Entities;
+
+namespace MemoryGame.Data
+{
+    /// <summary>
+    ///  Единственная база данных изображений
+    /// </summary>
+    public class DataBaseContext
+    {
+        private static DataBaseContext _instance;
+        public static DataBaseContext GetInstance() => _instance ?? (_instance = new DataBaseContext());
+
+        public List<Image> Images { get; set; }
+
+        public DataBaseContext()
+        {
+            Images = new List<Image>
+            {
+                new Image{FileName = @""}, //Пустая карта
+            };
+        }
+    }
+}
